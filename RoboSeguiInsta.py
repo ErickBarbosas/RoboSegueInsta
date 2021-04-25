ebf4aceda6cfbdfb41efd3eb27a0c9cb3928794e
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-04-25 16:35:52 -0300

    Inicio Projeto

---
+++ RoboSeguiInsta.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+
+driver.get('https://www.youtube.com/watch?v=ow8514gnajw')
