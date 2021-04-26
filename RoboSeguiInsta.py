b104d3e94f79e217f0ee7e9b150dd6737e5e0340
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-04-25 21:21:30 -0300

    Criando disparos para seguir pessoas

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -1,5 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
+
+
+links=[]
+valor=[]
+
+#perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+
+# links=perfis.split()
+
+
+
+i = 0
+#while True:
+    
 driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+driver.get('https://www.instagram.com/amante_dos_baixos/')
+sleep(3)
 
-driver.get('https://www.youtube.com/watch?v=ow8514gnajw')
+#Clicando no botao de login   
+driver.find_element_by_css_selector("button[type='button']").click()
+#Digitando Login e senha 
+driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
+driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
+#Clicando no botao logar
+driver.find_element_by_css_selector("button[type='submit']").click()
+sleep(5)
+#Clicando para nao salvar informacoes no navegador
+driver.find_element_by_xpath("//button[text()='Agora não']").click()
+#Clicando no botao de seguidores no perfil
+driver.find_element_by_xpath("//a[text()=' seguidores']").click()
+
+#Clicando em Seguir
+
+try:
+    while True:
+        sleep(3)
+        valor= driver.find_element_by_xpath("//button[text()='Seguir']").click()
+       
+
+except:
+    print("erro")
+
