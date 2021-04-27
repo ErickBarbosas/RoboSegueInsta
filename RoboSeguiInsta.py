244a32dd0f81651050485fbe02f78a7cb0313f50
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-04-26 21:27:27 -0300

    Implementação de um menu e a ação de deixar de seguir perfil

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -2,45 +2,85 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-
-
 links=[]
 valor=[]
 
-#perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+print('**************************************')
+print('*****  Robo Ganhe Seguidores :) ******')
+print('**************************************')
+print('               MENU                   ')
+print('\nSelecione uma opção')
+print('1- Seguir Seguidores de um Perfil')
+print('2- Deixar De Seguir')
 
-# links=perfis.split()
+opcao = str(input('R: '))
 
+if opcao =='1':
 
+    perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+    seguir=int(input('Digite Quantos Perfil Você que Seguir\n'))
 
-i = 0
-#while True:
-    
-driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
-driver.get('https://www.instagram.com/amante_dos_baixos/')
-sleep(3)
+    links=perfis.split()
 
-#Clicando no botao de login   
-driver.find_element_by_css_selector("button[type='button']").click()
-#Digitando Login e senha 
-driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
-driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
-#Clicando no botao logar
-driver.find_element_by_css_selector("button[type='submit']").click()
-sleep(5)
-#Clicando para nao salvar informacoes no navegador
-driver.find_element_by_xpath("//button[text()='Agora não']").click()
-#Clicando no botao de seguidores no perfil
-driver.find_element_by_xpath("//a[text()=' seguidores']").click()
+    for i in range(len(links)):
+            driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+            driver.get(links[i])
+        
+            #Clicando no botao de login   
+            driver.find_element_by_css_selector("button[type='button']").click()
+            #Digitando Login e senha 
+            driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
+            driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
+            #Clicando no botao logar
+            driver.find_element_by_css_selector("button[type='submit']").click()
+            sleep(5)
+            #Clicando para nao salvar informacoes no navegador
+            driver.find_element_by_xpath("//button[text()='Agora não']").click()
 
-#Clicando em Seguir
+            #Clicando no botao de seguidores no perfil
+            driver.find_element_by_xpath("//a[text()=' seguidores']").click()
 
-try:
-    while True:
+            try:
+               for j in seguir:
+                    for i in range(5):
+                        sleep(3)
+                        #Clicando em Seguir
+                        valor= driver.find_element_by_xpath("//button[text()='Seguir']").click()
+                    sleep(60)
+
+            except:
+                print("erro")
+
+elif opcao == '2':
+
+    perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+    seguir=int(input('Digite Quantos Perfil Deseja Deixar de Seguir\n'))
+    links=perfis.split()
+
+    for i in range(len(links)):
+        driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+        driver.get(links[i])
+        
         sleep(3)
-        valor= driver.find_element_by_xpath("//button[text()='Seguir']").click()
-       
+        #Clicando no botao de login   
+        driver.find_element_by_css_selector("button[type='button']").click()
+        #Digitando Login e senha 
+        driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
+        driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
+        #Clicando no botao logar
+        driver.find_element_by_css_selector("button[type='submit']").click()
+        sleep(5)
+        #Clicando para nao salvar informacoes no navegador
+        driver.find_element_by_xpath("//button[text()='Agora não']").click()
+        #Clicando no botao de seguidores no perfil
+        driver.find_element_by_xpath("//a[text()=' seguindo']").click()
 
-except:
-    print("erro")
-
+        #Deixando de segui Usuario
+        for j in range(seguir):
+            sleep(3)
+            driver.find_element_by_xpath("//button[text()='Seguindo']").click()
+            sleep(2)
+            driver.find_element_by_xpath("//button[text()='Deixar de seguir']").click()
+                
+elif opcao != '1' or opcao != '2':
+    print('Valor Invalido')
