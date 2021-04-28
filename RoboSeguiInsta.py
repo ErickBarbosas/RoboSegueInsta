4fec37672bd88c2438682bfa83ff6346f4b76496
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-04-27 22:34:31 -0300

    incluindo Login e senha

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import os
 from time import sleep
 
 links=[]
 valor=[]
 
+os.system('cls') or None #Limpa Tela
 print('**************************************')
 print('*****  Robo Ganhe Seguidores :) ******')
 print('**************************************')
@@ -16,35 +17,41 @@ print('2- Deixar De Seguir')
 opcao = str(input('R: '))
 
 if opcao =='1':
+  
+    os.system('cls') or None #Limpa Tela 
 
+    #Digite o link da pagina que deseja seguir os seguidores
     perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+    #Quantidades de perfil que deseja seguir
     seguir=int(input('Digite Quantos Perfil Você que Seguir\n'))
-
+   
+    #login do Instagram
+    login=str(input('Digite seu Login: '))
+    #Senha Do perfil
+    senha=str(input('Digite a Senha do Perfil: '))
     links=perfis.split()
 
     for i in range(len(links)):
+           
+            os.system('cls') or None #Limpa Tela
+            print('Iniciando Processo...')
+            #WebDriver
             driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+            #Abrindo navegados e entrando no link
             driver.get(links[i])
             sleep(3)
 
             #Clicando no botao de login
             driver.find_element_by_css_selector("button[type='button']").click()
-<<<<<<< HEAD
             sleep(1)
             #Digitando Login e senha
-            driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
-            driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
-=======
-            #Digitando Login e senha 
-            driver.find_element_by_css_selector("input[name='username']").send_keys('')
-            driver.find_element_by_css_selector("input[name='password']").send_keys('')
->>>>>>> c5e7307327b937047cac89613f075eb542d2e360
+            driver.find_element_by_css_selector("input[name='username']").send_keys(login)
+            driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
             #Clicando no botao logar
             driver.find_element_by_css_selector("button[type='submit']").click()
             sleep(5)
             #Clicando para nao salvar informacoes no navegador
             driver.find_element_by_xpath("//button[text()='Agora não']").click()
-
             #Clicando no botao de seguidores no perfil
             driver.find_element_by_xpath("//a[text()=' seguidores']").click()
 
@@ -64,27 +71,40 @@ if opcao =='1':
 
 elif opcao == '2':
 
+    
+
+    #Digite o link da pagina que deseja seguir os seguidores
+    os.system('cls') or None #Limpa Tela 
     perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
-    seguir=int(input('Digite Quantos Perfil Deseja Deixar de Seguir\n'))
+   
+    #Quantidades de perfil que deseja seguir
+    os.system('cls') or None #Limpa Tela 
+    seguir=int(input('Digite Quantos Perfil Você Deseja Deixar de Seguir: \n'))
+   
+    #login do Instagram
+    login=str(input('Digite seu Login: '))
+    #Senha Do perfil
+    senha=str(input('Digite a Senha do Perfil: '))
+
     links=perfis.split()
 
     for i in range(len(links)):
+
+        os.system('cls') or None #Limpa Tela 
+        print('Iniciando Processo...')
+
         driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
         driver.get(links[i])
         sleep(3)
 
         #Clicando no botao de login
         driver.find_element_by_css_selector("button[type='button']").click()
-<<<<<<< HEAD
+
         sleep(1)
         #Digitando Login e senha
-        driver.find_element_by_css_selector("input[name='username']").send_keys('hermanos_acessorios')
-        driver.find_element_by_css_selector("input[name='password']").send_keys('Quiksilver2@')
-=======
-        #Digitando Login e senha 
-        driver.find_element_by_css_selector("input[name='username']").send_keys('')
-        driver.find_element_by_css_selector("input[name='password']").send_keys('')
->>>>>>> c5e7307327b937047cac89613f075eb542d2e360
+        driver.find_element_by_css_selector("input[name='username']").send_keys(login)
+        driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
+
         #Clicando no botao logar
         driver.find_element_by_css_selector("button[type='submit']").click()
         sleep(5)
