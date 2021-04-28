555717bd0e0bd57d16aeea44b0f001aa7ec878a1
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-04-28 17:24:55 -0300

    Difionando Função Main

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -2,37 +2,39 @@ from selenium import webdriver
 import os
 from time import sleep
 
-links=[]
-valor=[]
+def main():
 
-os.system('cls') or None #Limpa Tela
-print('**************************************')
-print('*****  Robo Ganhe Seguidores :) ******')
-print('**************************************')
-print('               MENU                   ')
-print('\nSelecione uma opção')
-print('1- Seguir Seguidores de um Perfil')
-print('2- Deixar De Seguir')
+    links=[]
+    valor=[]
 
-opcao = str(input('R: '))
+    os.system('cls') or None #Limpa Tela
+    print('**************************************')
+    print('*****  Robo Ganhe Seguidores :) ******')
+    print('**************************************')
+    print('               MENU                   ')
+    print('\nSelecione uma opção')
+    print('1- Seguir Seguidores de um Perfil')
+    print('2- Deixar De Seguir')
 
-if opcao =='1':
-  
-    os.system('cls') or None #Limpa Tela 
+    opcao = str(input('R: '))
 
-    #Digite o link da pagina que deseja seguir os seguidores
-    perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
-    #Quantidades de perfil que deseja seguir
-    seguir=int(input('Digite Quantos Perfil Você que Seguir\n'))
-   
-    #login do Instagram
-    login=str(input('Digite seu Login: '))
-    #Senha Do perfil
-    senha=str(input('Digite a Senha do Perfil: '))
-    links=perfis.split()
+    if opcao =='1':
+    
+        os.system('cls') or None #Limpa Tela 
 
-    for i in range(len(links)):
-           
+        #Digite o link da pagina que deseja seguir os seguidores
+        perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+        #Quantidades de perfil que deseja seguir
+        seguir=int(input('Digite Quantos Perfil Você que Seguir\n'))
+        
+        #login do Instagram
+        login=str(input('Digite seu Login: '))
+        #Senha Do perfil
+        senha=str(input('Digite a Senha do Perfil: '))
+        links=perfis.split()
+
+        for i in range(len(links)):
+                
             os.system('cls') or None #Limpa Tela
             print('Iniciando Processo...')
             #WebDriver
@@ -62,63 +64,66 @@ if opcao =='1':
                     driver.find_element_by_xpath("//button[text()='Seguir']").click()
 
                     contador +=1
-                    if contador ==5:
-                        contador=0
-                        sleep(60)
+                if contador ==5:
+                    contador=0
+                    sleep(60)
 
             except:
                 print("Erro:")
+    elif opcao == '2':
 
-elif opcao == '2':
+            
 
-    
+            #Digite o link da pagina que deseja seguir os seguidores
+            os.system('cls') or None #Limpa Tela 
+            perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
+        
+            #Quantidades de perfil que deseja seguir
+            os.system('cls') or None #Limpa Tela 
+            seguir=int(input('Digite Quantos Perfil Você Deseja Deixar de Seguir: \n'))
+        
+            #login do Instagram
+            login=str(input('Digite seu Login: '))
+            #Senha Do perfil
+            senha=str(input('Digite a Senha do Perfil: '))
 
-    #Digite o link da pagina que deseja seguir os seguidores
-    os.system('cls') or None #Limpa Tela 
-    perfis= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
-   
-    #Quantidades de perfil que deseja seguir
-    os.system('cls') or None #Limpa Tela 
-    seguir=int(input('Digite Quantos Perfil Você Deseja Deixar de Seguir: \n'))
-   
-    #login do Instagram
-    login=str(input('Digite seu Login: '))
-    #Senha Do perfil
-    senha=str(input('Digite a Senha do Perfil: '))
+            links=perfis.split()
 
-    links=perfis.split()
+            for i in range(len(links)):
 
-    for i in range(len(links)):
+                os.system('cls') or None #Limpa Tela 
+                print('Iniciando Processo...')
 
-        os.system('cls') or None #Limpa Tela 
-        print('Iniciando Processo...')
+                driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+                driver.get(links[i])
+                sleep(3)
 
-        driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
-        driver.get(links[i])
-        sleep(3)
+                #Clicando no botao de login
+                driver.find_element_by_css_selector("button[type='button']").click()
 
-        #Clicando no botao de login
-        driver.find_element_by_css_selector("button[type='button']").click()
+                sleep(1)
+                #Digitando Login e senha
+                driver.find_element_by_css_selector("input[name='username']").send_keys(login)
+                driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
 
-        sleep(1)
-        #Digitando Login e senha
-        driver.find_element_by_css_selector("input[name='username']").send_keys(login)
-        driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
+                #Clicando no botao logar
+                driver.find_element_by_css_selector("button[type='submit']").click()
+                sleep(5)
+                #Clicando para nao salvar informacoes no navegador
+                driver.find_element_by_xpath("//button[text()='Agora não']").click()
+                #Clicando no botao de seguidores no perfil
+                driver.find_element_by_xpath("//a[text()=' seguindo']").click()
 
-        #Clicando no botao logar
-        driver.find_element_by_css_selector("button[type='submit']").click()
-        sleep(5)
-        #Clicando para nao salvar informacoes no navegador
-        driver.find_element_by_xpath("//button[text()='Agora não']").click()
-        #Clicando no botao de seguidores no perfil
-        driver.find_element_by_xpath("//a[text()=' seguindo']").click()
+                #Deixando de segui Usuario
+                for j in range(seguir):
+                    sleep(3)
+                    driver.find_element_by_xpath("//button[text()='Seguindo']").click()
+                    sleep(2)
+                    driver.find_element_by_xpath("//button[text()='Deixar de seguir']").click()
 
-        #Deixando de segui Usuario
-        for j in range(seguir):
-            sleep(3)
-            driver.find_element_by_xpath("//button[text()='Seguindo']").click()
-            sleep(2)
-            driver.find_element_by_xpath("//button[text()='Deixar de seguir']").click()
+    elif opcao != '1' or opcao != '2':
+            print('Valor Invalido')
 
-elif opcao != '1' or opcao != '2':
-    print('Valor Invalido')
+
+if __name__ == '__main__':
+    main()
