2fe84d29da8595ec1a6f2027b58a798f95bbd403
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-05-04 20:57:39 -0300

    Refaturando Codigo

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -4,38 +4,34 @@ from selenium.webdriver.common.keys import Keys
 import os
 from time import sleep
 
-seguir = 0
-contador = 0
-login = ""
-senha = ""
-links= ""
-valor=[]
-
-def LogarUsuario():
-    driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
-    #Abrindo navegados e entrando no link
-    driver.get(links)
-    sleep(3)
-
-    #Clicando no botao de login
-    driver.find_element_by_css_selector("button[type='button']").click()
-    sleep(1)
-    #Digitando Login e senha
-    driver.find_element_by_css_selector("input[name='username']").send_keys(login)
-    driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
-    #Clicando no botao logar
-    driver.find_element_by_css_selector("button[type='submit']").click()
-    sleep(5)
-    #Clicando para nao salvar informacoes no navegador
-    driver.find_element_by_xpath("//button[text()='Agora não']").click()
-
-
-    
-
 def main():
+    seguir = 0
+    contador = 0
+    login = ""
+    senha = ""
+    links= ""
+    valor=[]
 
-   
     
+    def LogarUsuario():
+        driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+        #Abrindo navegados e entrando no link
+        driver.get(links)
+        sleep(3)
+        #Clicando no botao de login
+        driver.find_element_by_css_selector("button[type='button']").click()
+        sleep(1)
+        #Digitando Login e senha
+        driver.find_element_by_css_selector("input[name='username']").send_keys(login)
+        driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
+        #Clicando no botao logar
+        driver.find_element_by_css_selector("button[type='submit']").click()
+        sleep(5)
+        #Clicando para nao salvar informacoes no navegador
+        driver.find_element_by_xpath("//button[text()='Agora não']").click()
+
+
+
     os.system('cls') or None #Limpa Tela
     print('**************************************')
     print('*****  Robo Ganhe Seguidores :) ******')
@@ -47,11 +43,10 @@ def main():
 
     opcao = str(input('R: '))
 
+    
     if opcao =='1':
         
-        contador = 0
         os.system('cls') or None #Limpa Tela 
-
         #Digite o link da pagina que deseja seguir os seguidores
         links= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
         #Quantidades de perfil que deseja seguir
