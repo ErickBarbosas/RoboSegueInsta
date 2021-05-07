768f607d2b7bb865b3d6bbee7b7d6768775c5804
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-05-06 21:31:39 -0300

    Criada nova função

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import selenium as sele
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import os
@@ -24,31 +23,37 @@ def main():
         login=str(input('Digite seu Login: '))
         #Senha Do perfil
         senha=str(input('Digite a Senha do Perfil: '))
+
+        #Abrindo navegados e entrando no link
+        os.system('cls') or None
         print('Iniciando Processo...')        
         driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
-        #Abrindo navegados e entrando no link
         driver.get(links)
         sleep(3)
+
         #Clicando no botao de login
         driver.find_element_by_css_selector("button[type='button']").click()
         sleep(1)
+
         #Digitando Login e senha
         driver.find_element_by_css_selector("input[name='username']").send_keys(login)
         driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
+
         #Clicando no botao logar
         driver.find_element_by_css_selector("button[type='submit']").click()
         sleep(5)
+
         #Clicando para nao salvar informacoes no navegador
         driver.find_element_by_xpath("//button[text()='Agora não']").click()
+        sleep(3) 
 
     def SeguirSeguidores():    
         global driver
         global contador
         
-
-        sleep(3) 
         #Clicando no botao de seguidores no perfil
         driver.find_element_by_xpath("//a[text()=' seguidores']").click()
+
         #Seguindo
         try:
             j = 0
@@ -56,7 +61,7 @@ def main():
                 sleep(3)
                 driver.find_element_by_xpath("//button[text()='Seguir']").click()
                
-                contador+=1
+               # contador+=1
                 j +=1
             if j ==5:
                 j=0
@@ -78,12 +83,25 @@ def main():
                 sleep(5)
                 driver.find_element_by_xpath("//button[text()='Deixar de seguir']").click()
                 global contador
-                contador+=1
+               # contador+=1
         except WebDriverException as Erro:
             os.system('cls') or None
             print('Ocorreu Um Erro no Processo:\n\n{}'.format(Erro))
 
-        
+    def FimProcesso():
+
+        print('****************************************************')    
+        print('*                   Fim do Processo                *')
+        print('****************************************************')
+        print('****************      Meta: {} Perfis **************'.format(seguir))
+        print('**************** Alcançado: {} Perfis **************'.format(contador))
+        print('****************************************************')
+        print('****************************************************\n')
+            
+        resposta =input('\nVocê Deseja Voltar Para o Menu Inicial?\n\n1-Sim\n2-Não\n\n')
+        if resposta == '1':
+            main()
+
         
 
 
@@ -109,53 +127,21 @@ def main():
         
         LogarUsuario()
         SeguirSeguidores()
+        FimProcesso()
 
-           
-        print('****************************************************')    
-        print('*           Fim do Processo (Seguir Perfis)        *')
-        print('****************************************************')
-        print('****************      Meta: {} Perfis **************'.format(seguir))
-        print('**************** Alcançado: {} Perfis **************'.format(contador))
-        print('****************************************************')
-        print('****************************************************\n')
-            
-        resposta =input('\nVocê Deseja Voltar Para o Menu Inicial?\n\n1-Sim\n2-Não\n\n')
-        if resposta == '1':
-            main()
-        
+    elif opcao == '2':      
 
-    elif opcao == '2':            
         #Digite o link da pagina que deseja seguir os seguidores
-        os.system('cls') or None #Limpa Tela 
+        os.system('cls') or None 
         links= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
     
         #Quantidades de perfil que deseja seguir
-        os.system('cls') or None #Limpa Tela 
+        os.system('cls') or None 
         seguir=int(input('Digite Quantos Perfil Você Deseja Deixar de Seguir: \n'))
 
         LogarUsuario()
         DeixardeSegui()
-        # #login do Instagram
-        # os.system('cls') or None #Limpa Tela 
-        # login=str(input('Digite seu Login: '))
-        # #Senha Do perfil
-        # senha=str(input('Digite a Senha do Perfil: '))
-
-        # links=perfis.split()
-
-        driver.close()  
-        print('****************************************************')  
-        print('*      Fim do Processo (Deixar de Segui Perfis)    *')
-        print('****************************************************')
-        print('****************      Meta: {} Perfis **************'.format(seguir))
-        print('**************** Alcançado: {} Perfis **************'.format(contador))
-        print('****************************************************')
-        print('****************************************************\n\n')
-        
-        resposta =input('\nVocê Deseja Voltar Para o Menu Inicial?\n\n1-Sim\n2-Não\n\n')
-        if resposta == '1':
-            main()
-
+        FimProcesso()
 
     elif opcao != '1' or opcao != '2':
             print('Valor Invalido')
