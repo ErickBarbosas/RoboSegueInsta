f9b0e866d8bd9c94765cef80b5f90c86a17addca
Erick Santos Barbosa <santoserick2015@hotmail.com>
2021-05-16 18:08:27 -0300

    Adicionado Funcao que fecha o navegador

--- RoboSeguiInsta.py
+++ RoboSeguiInsta.py
@@ -1,3 +1,4 @@
+from logging import error
 from selenium import webdriver
 import selenium
 from selenium.common.exceptions import WebDriverException
@@ -20,49 +21,54 @@ def main():
 
     def LogarUsuario():
         global driver
+        try:
+            #login do Instagram
+            login=str(input('Digite seu Login: '))
+            #Senha Do perfil
+            senha=str(input('Digite a Senha do Perfil: '))
 
-        #login do Instagram
-        login=str(input('Digite seu Login: '))
-        #Senha Do perfil
-        senha=str(input('Digite a Senha do Perfil: '))
+            #Abrindo navegados e entrando no link
+            os.system('cls') or None
+            print('Iniciando Processo...')        
+            driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
+            driver.maximize_window() #Maximizar Page
+            driver.get(links) #Abrir Link
+            
+            sleep(3)
+            
+            #Clicando no botao de login
+            driver.find_element_by_css_selector("button[type='button']").click()
+            sleep(1)
 
-        #Abrindo navegados e entrando no link
-        os.system('cls') or None
-        print('Iniciando Processo...')        
-        driver = webdriver.Chrome(executable_path="C:\Projetos\RoboSeguiInsta\webdriver\chromedriver.exe")
-        driver.maximize_window() #Maximizar Page
-        driver.get(links) #Abrir Link
-        
-        sleep(3)
-        
-        #Clicando no botao de login
-        driver.find_element_by_css_selector("button[type='button']").click()
-        sleep(1)
+            #Digitando Login e senha
+            driver.find_element_by_css_selector("input[name='username']").send_keys(login)
+            driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
 
-        #Digitando Login e senha
-        driver.find_element_by_css_selector("input[name='username']").send_keys(login)
-        driver.find_element_by_css_selector("input[name='password']").send_keys(senha)
+            #Clicando no botao logar
+            driver.find_element_by_css_selector("button[type='submit']").click()
+            sleep(5)
 
-        #Clicando no botao logar
-        driver.find_element_by_css_selector("button[type='submit']").click()
-        sleep(5)
+            #Clicando para nao salvar informacoes no navegador
+            driver.find_element_by_xpath("//button[text()='Agora não']").click()
+            sleep(3) 
+        except WebDriverException as Erro:
+            driver.quit()
+            os.system('cls') or None
+            print('Ocorreu Um Erro no Processo:\n\n{}'.format(Erro))
 
-        #Clicando para nao salvar informacoes no navegador
-        driver.find_element_by_xpath("//button[text()='Agora não']").click()
-        sleep(3) 
  
 
     def SeguirSeguidores():    
         global driver
         global contador
         count = 0
-       
-        #Clicando no botao de seguidores no perfil
-        driver.find_element_by_xpath("//a[text()=' seguidores']").click()
-        sleep(5)
-        
-        #Seguindo
         try:
+            #Clicando no botao de seguidores no perfil
+            driver.find_element_by_xpath("//a[text()=' seguidores']").click()
+            sleep(5)
+            
+            #Seguindo
+        
             j = 0
             for i in range(seguir):
                 sleep(5)
@@ -74,6 +80,7 @@ def main():
                     j=0
                     sleep(70)    
         except WebDriverException as Erro:
+            driver.quit()
             os.system('cls') or None
             print("Ocorreu Um Erro: \n\n{}".format(Erro))
         
@@ -85,13 +92,14 @@ def main():
             global driver
             global contador
             count = 0
-            
-            driver.execute_script("window.scrollBy(0,20)")
-            sleep(10)
+
             #Clicando no botao de seguidores no perfil
             driver.find_element_by_xpath("//a[text()=' seguindo']").click()
-           
+            sleep(3)
             
+            driver.execute_script("window.scrollBy(0,20)")
+            sleep(1)
+
             for j in range(seguir):
                 sleep(5)
                 driver.find_element_by_xpath("//button[text()='Seguindo']").click()
@@ -101,6 +109,7 @@ def main():
                 count+=1
                 
         except WebDriverException as Erro:
+            driver.quit()
             os.system('cls') or None
             print('Ocorreu Um Erro no Processo:\n\n{}'.format(Erro))
 
@@ -145,6 +154,7 @@ def main():
         #Digite o link da pagina que deseja seguir os seguidores
         links= str(input('Digite os Links do Perfil (Separados Por ;):\n'))
         #Quantidades de perfil que deseja seguir
+        os.system('cls') or None #Limpa Tela 
         seguir=int(input('Digite Quantos Perfil Você que Seguir\n'))
 
         LogarUsuario()
